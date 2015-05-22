ba7a30268ef4d2e79ebf1c1b3d1ed95841c8d268
wayne <wayhap@gmail.com>
2015-05-22 15:09:55 +0800

    update FocusImageFragment

--- src/cn/way/wandroid/imageloader/loader/FocusImageFragment.java
+++ src/cn/way/wandroid/imageloader/loader/FocusImageFragment.java
@@ -8,27 +8,17 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import cn.way.wandroid.R;
-import cn.way.wandroid.imageloader.loader.ImageLoader;
-import cn.way.wandroid.imageloader.loader.ImageWorker;
+import cn.way.wandroid.imageloader.ImageManager;
 
 public class FocusImageFragment extends Fragment {
     private static final String IMAGE_DATA_EXTRA = "extra_image_data";
     private String mImageUrl;
     private ImageView mImageView;
-    private ImageLoader imageLoader;
     private int layoutId;
     private OnClickListener clickListener;
-    public ImageLoader getImageLoader() {
-		return imageLoader;
-	}
 
-	public void setImageLoader(ImageLoader imageLoader) {
-		this.imageLoader = imageLoader;
-	}
-
-    public static FocusImageFragment newInstance(int layoutId,ImageLoader imageLoader,String imageUrl,OnClickListener clickListener) {
+    public static FocusImageFragment newInstance(int layoutId,String imageUrl,OnClickListener clickListener) {
         final FocusImageFragment f = new FocusImageFragment();
-        f.setImageLoader(imageLoader);
         f.setLayoutId(layoutId);
         f.setClickListener(clickListener);
         final Bundle args = new Bundle();
@@ -65,8 +55,8 @@ public class FocusImageFragment extends Fragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        if (imageLoader!=null) {
-            imageLoader.loadImage(mImageUrl, mImageView);
+        if (mImageUrl!=null&&mImageView!=null) {
+            ImageManager.instance(getActivity()).loadImage(mImageUrl, mImageView);
         }
     }
 
@@ -74,8 +64,6 @@ public class FocusImageFragment extends Fragment {
     public void onDestroy() {
         super.onDestroy();
         if (mImageView != null) {
-            // Cancel any pending image work
-            ImageWorker.cancelWork(mImageView);
             mImageView.setImageDrawable(null);
         }
     }
